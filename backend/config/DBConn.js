71249fd86a32c891f1e4b13a4406bff4f6f7ab6b
Mushfiqur Niaz <mushfiqurniaz33.com>
2024-05-24 00:17:55 +0600

    added password field

--- backend/config/DBConn.js
+++ backend/config/DBConn.js
@@ -30,6 +30,7 @@ const DBConn = async () => {
             id INT AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(50) NOT NULL UNIQUE,
             email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`
     );
