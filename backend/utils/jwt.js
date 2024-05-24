db1dfc0ec79b5a09fa498f42d4b8961bdb7883e6
Mushfiqur Niaz <mushfiqurniaz33.com>
2024-05-24 19:16:56 +0600

    fixed jwt

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,13 +2,13 @@
 const jwt = require("jsonwebtoken");
 
 //main token function that will be sending the token with some information init
-const token = (foundUSer, response) => {
+const token = (foundUser, response) => {
   //creating a jwt with the user's id, username, email and environmental variables
   const jwtToken = jwt.sign(
     {
-      id: foundUSer.id,
-      username: foundUSer.username,
-      email: foundUSer.email
+      id: foundUser.id,
+      username: foundUser.username,
+      email: foundUser.email
     },
     process.env.JWT_SECRET,
     {
