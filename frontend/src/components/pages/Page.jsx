16c4c9c30011111b26b1402a9df6a172abc9bacf
Mushfiqur Niaz <mushfiqurniaz33.com>
2024-05-29 21:24:29 +0600

    created ui for 404 and / page

--- frontend/src/components/pages/Page.jsx
+++ frontend/src/components/pages/Page.jsx
@@ -1,11 +1,20 @@
 //this is a simple 404 page with some custom styling
-import React from "react";
 import styles from "../styles/Page.module.css";
 
 function Page() {
   return (
-  <div>404Page</div>
-);
+    <>
+      <div className={styles.container}>
+        <div id={styles.div}>
+          <h1 id={styles.h1}>404</h1>
+          <hr />
+          <p id={styles.p}>
+            This page doesn&apos;t exist returning to &apos;HOME PAGE&apos;
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Page;
--- frontend/src/components/pages/SpecalPage.jsx
+++ frontend/src/components/pages/SpecalPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import styles from "../styles/Special.module.css";
 
-function SpecalPage() {
+function SpecialPage() {
   return (
-    <div>SpecalPage</div>
-  )
+    <>
+      <div className={styles.container}>
+        <div id={styles.div}>
+          <h1 id={styles.h1}>Special Page</h1>
+          <hr />
+          <p id={styles.p}>
+            This is the special page which can only be accessed by a token.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default SpecalPage
+export default SpecialPage;
